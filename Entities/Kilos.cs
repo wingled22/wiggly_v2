bde28e53e88e018b1115ecf9e506de43fcf01c37
wingled22 <pelayowindel@gmail.com>
2022-03-14 13:55:56 +0800

    updated models

--- Entities/Kilos.cs
+++ Entities/Kilos.cs
@@ -7,10 +7,17 @@ namespace Wiggly.Entities
     {
         public int Id { get; set; }
         public int? Transaction { get; set; }
+        public int? Vendor { get; set; }
+        public int? Farmer { get; set; }
+        public int? PorkNum { get; set; }
         public double? Pork { get; set; }
+        public int? BeefNum { get; set; }
         public double? Beef { get; set; }
+        public int? ChickenNum { get; set; }
         public double? Chicken { get; set; }
+        public int? GoatNum { get; set; }
         public double? Goat { get; set; }
-        public double? Lamb { get; set; }
+        public int? CarabaoNum { get; set; }
+        public double? Carabao { get; set; }
     }
 }
