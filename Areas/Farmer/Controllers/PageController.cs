879b9501a34863164be7b42f0c281d56976c6c7c
wingled22 <pelayowindel@gmail.com>
2022-03-15 10:40:50 +0800

    updated farmer pages

---
+++ Areas/Farmer/Controllers/PageController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wiggly.Entities;
+using Wiggly.Identity;
+
+namespace Wiggly.Areas.Farmer.Controllers
+{
+    [Authorize(Roles = "Farmer")]
+    [Area("Farmer")]
+    public class PageController : Controller
+    {
+
+        private readonly ILogger<PageController> _logger;
+        private UserManager<AppUser> _usrMngr;
+        private SignInManager<AppUser> _signInMngr;
+        private readonly WigglyContext _context;
+        public PageController(WigglyContext context, UserManager<AppUser> usrMngr, SignInManager<AppUser> signInMngr, ILogger<PageController> logger)
+        {
+            _usrMngr = usrMngr;
+            _signInMngr = signInMngr;
+            _context = context;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+        public IActionResult Transaction()
+        {
+            return View();
+        }
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+    }
+}
